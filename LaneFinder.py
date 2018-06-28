76a49edd2879039ee791d0a2a97e0d47fbe742bd
JasonDuong1997 <jasonduong1997@gmail.com>
2018-06-28 12:58:58 -0800

    extracted the left and right slopes and added in an arrow as an indicator on whether to move left or right

--- LaneFinder.py
+++ LaneFinder.py
@@ -29,26 +29,27 @@ def Lines(image):   # draw HoughLines
 	gap_max = 30
 	lines = cv2.HoughLinesP(image, 1, np.pi/180, 100, np.array([]), line_min, gap_max)
 
-	lineAggregator = LineList()  # list to hold the most common lines
-	lineCounts = LineList()      # list to hold the number of each line
+	lineAggregator_L = LineList()  # list to hold the most common lines (LEFT LINE)
+	lineAggregator_R = LineList()  # list to hold the most common lines (RIGHT LINE)
+	lineCounts_L = LineList()      # list to hold the number of each line (LEFT LINE)
+	lineCounts_R = LineList()	   # list to hold the number of each line (RIGHT LINE)
 
 	try:    # add all lines to list and then pick out the 2 strongest lines
 		for line in lines:
 			coords = line[0]
-			PopulateLineLists(coords, lineAggregator)
+			PopulateLineLists(coords, lineAggregator_L, lineAggregator_R)
 
-		coord1_a, coord1_b, coord2_a, coord2_b = LineFilter(lineAggregator, lineCounts)
-		print(coord1_a)
-		print(coord1_b)
-		print(coord2_a[1])
-		print(coord2_b[1])
+		coord1_a, coord1_b, slope_L = LineFilter(lineAggregator_L, lineCounts_L)
+		coord2_a, coord2_b, slope_R = LineFilter(lineAggregator_R, lineCounts_R)
+
 		cv2.line(image, (coord1_a[0], coord1_a[1]), (coord1_b[0], coord1_b[1]), (255,255,255), thickness=2)
-		cv2.line(image, (coord2_a[0], coord2_a[1]), (coord2_b[0], coord2_b[1]), (255,255,255), thickness=2)
-		'''if (skip != 1):
-			cv2.line(image, (coords[0],coords[1]), (coords[2],coords[3]), (255,255,255), thickness=2)'''
+		cv2.line(image, (coord2_a[0], coord2_a[1]), (coord2_b[0], coord2_b[1]), (255,255,255), thickness=6)
+
+		return slope_L, slope_R
 	except Exception:
 		traceback.print_exc()
 		print("ERROR: No lines found")
+		return 0, 0
 
 
 def Region(image, vertices):    # only look at region of interest
@@ -58,7 +59,7 @@ def Region(image, vertices):    # only look at region of interest
 	return masked
 
 
-def PopulateLineLists(coords, lineAggregator):
+def PopulateLineLists(coords, lineAggregator_L, lineAggregator_R):
 	dY = -1 * (coords[3] - coords[1])  # inverting since origin point is at top of screen
 	dX = coords[2] - coords[0]
 
@@ -67,15 +68,14 @@ def PopulateLineLists(coords, lineAggregator):
 		slope = dY / dX
 		distance = math.sqrt(math.pow(dX, 2) + math.pow(dY, 2))
 
-		if (math.fabs(slope) >= .2):  # removing horizontal lines
+		if (slope >= .2):  # POPULATING LEFT LINE
 			print("Slope: {}".format(slope))
 			y_int = coords[3] + slope * coords[2]
 			print("Y-Intercept: {}".format(y_int))
 
 			try:  # add slope and y-intercept to list
-				lineAggregator.addLine([slope, y_int])
-				print("AGGREGATOR SIZE: {}".format(lineAggregator.size))
-			   # print("AGGREGATOR: {}".format(lineAggregator))
+				lineAggregator_L.addLine([slope, y_int])
+				print("AGGREGATOR SIZE: {}".format(lineAggregator_L.size))
 			except:
 				print("ERROR: Could not add line")
 
@@ -83,10 +83,27 @@ def PopulateLineLists(coords, lineAggregator):
 			print("dX = {}   dY = {}".format(dX, dY))
 			print("This is the slope: {} ".format(dY / dX))
 			print("This is distance: {} ".format(distance))
-		else:
+
+		elif (slope <= -.2):  # POPULATING RIGHT LINE
+			print("Slope: {}".format(slope))
+			y_int = coords[3] + slope * coords[2]
+			print("Y-Intercept: {}".format(y_int))
+
+			try:  # add slope and y-intercept to list
+				lineAggregator_R.addLine([slope, y_int])
+				print("AGGREGATOR SIZE: {}".format(lineAggregator_R.size))
+			# print("AGGREGATOR: {}".format(lineAggregator))
+			except:
+				print("ERROR: Could not add line")
+
+			# DEBUG printing
+			print("dX = {}   dY = {}".format(dX, dY))
+			print("This is the slope: {} ".format(dY / dX))
+			print("This is distance: {} ".format(distance))
+		else:  # if horizontal line
 			print("NOT PRINTING HORIZONTAL LINE")
 			return 1
-	else:
+	else:  # if denominator is 0
 		print("ERROR: dX = 0. Skipping")
 
 
@@ -97,6 +114,7 @@ def LineFilter(lineAggregator, lineCounts):  # remove unwanted lines
 	search = True
 	i = 0
 	while (search):    # populates the repeating lines into lineCount list in the format [index, count]
+		print("AGGREGATOR SIZE {} and I = {}" .format(lineAggregator.size, i))
 		slope1 = lineAggregator[i][0]
 		y_int1 = lineAggregator[i][1]
 		count = 1
@@ -122,7 +140,7 @@ def LineFilter(lineAggregator, lineCounts):  # remove unwanted lines
 	lineCounts.sort(key=lineCountMax, reverse=True)
 	print("SORTED LINE COUNT: {}" .format(lineCounts))
 
-	# calculate the average of the top 2 strongest lines
+	# calculate the average of the strongest line
 	slope_avg1 = 0
 	y_int_avg1 = 0
 	index_start1 = lineCounts[0][0]
@@ -134,18 +152,7 @@ def LineFilter(lineAggregator, lineCounts):  # remove unwanted lines
 	y_int_avg1 /= (index_end1 - index_start1)
 	print("SLOPE1: {}       Y-INT: {}" .format(slope_avg1, y_int_avg1))
 
-	slope_avg2 = 0
-	y_int_avg2 = 0
-	index_start2 = lineCounts[1][0]
-	index_end2 = index_start2 + lineCounts[1][1]
-	for i in range(index_start2, index_end2):
-		slope_avg2 += lineAggregator[i][0]
-		y_int_avg2 += lineAggregator[i][1]
-	slope_avg2 /= (index_end2 - index_start2)
-	y_int_avg2 /= (index_end2 - index_start2)
-	print("SLOPE2: {}       Y-INT: {}" .format(slope_avg2, y_int_avg2))
-
-	#calculating coordinates of the two lines
+	#calculating coordinates of the line
 	width,height = pag.size()
 	print("WIDTH {}    HEIGHT {}" .format(width, height))
 	x1_bottom = -(height - y_int_avg1)/slope_avg1
@@ -153,12 +160,7 @@ def LineFilter(lineAggregator, lineCounts):  # remove unwanted lines
 	coord1_a = [int(x1_bottom), int(height)]
 	coord1_b = [int(x1_top), 0]
 
-	x2_bottom = -(height - y_int_avg2)/slope_avg2
-	x2_top = y_int_avg2/slope_avg2
-	coord2_a = [int(x2_bottom), int(height)]
-	coord2_b = [int(x2_top), int(0)]
-
-	return coord1_a, coord1_b, coord2_a, coord2_b
+	return coord1_a, coord1_b, slope_avg1
 
 
 def lineCountMax(lineCounts):
@@ -182,7 +184,12 @@ def main():
 
 			image2 = Edge(image)
 			image2 = Region(image2, [vertices])
-			Lines(image2)
+			slope_L, slope_R = Lines(image2)
+
+			if (math.fabs(slope_L) > math.fabs(slope_R)):
+				cv2.arrowedLine(image2, (20, 20), (40, 20), (255, 255, 255), thickness=2, tipLength=.3)
+			elif (math.fabs(slope_L) < math.fabs(slope_R)):
+				cv2.arrowedLine(image2, (40, 20), (20, 20), (255, 255, 255), thickness=2, tipLength=.3)
 
 			cv2.imshow("Image", image2)
 

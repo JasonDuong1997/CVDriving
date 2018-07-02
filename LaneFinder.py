91cc6ece686dea19cfb53058a51457b500a98fe3
JasonDuong1997 <jasonduong1997@gmail.com>
2018-07-02 00:25:03 -0800

    -added dynamic ROI height

--- LaneFinder.py
+++ LaneFinder.py
@@ -31,18 +31,9 @@ def Lines(image):   # draw HoughLines
 		line_min = 40
 		gap_max = 50
 	else:
-		line_min = 50
+		line_min = 30
 		gap_max = 60
 	lines = cv2.HoughLinesP(image, 1, np.pi/180, 100, np.array([]), line_min, gap_max)
-
-	'''
-	# //////////// TESTING
-	for line in lines:
-		coords = line[0]
-		cv2.line(image, (coords[0], coords[1]), (coords[2], coords[3]), (255,255,255), thickness=2)
-	return 0,0
-	# //////////////////
-	'''
 
 	lineAggregator_L = LineList()  # list to hold the most common lines (LEFT LINE)
 	lineAggregator_R = LineList()  # list to hold the most common lines (RIGHT LINE)
@@ -54,17 +45,20 @@ def Lines(image):   # draw HoughLines
 			coords = line[0]
 			PopulateLineLists(coords, lineAggregator_L, lineAggregator_R)
 
-		coord1_a, coord1_b, slope_L = LineFilter(lineAggregator_L, lineCounts_L)
-		coord2_a, coord2_b, slope_R = LineFilter(lineAggregator_R, lineCounts_R)
+		coord1_a, coord1_b, slope_L, y_int_L = LineFilter(lineAggregator_L, lineCounts_L)
+		coord2_a, coord2_b, slope_R, y_int_R = LineFilter(lineAggregator_R, lineCounts_R)
 
 		cv2.line(image, (coord1_a[0], coord1_a[1]), (coord1_b[0], coord1_b[1]), (255,255,255), thickness=2)
 		cv2.line(image, (coord2_a[0], coord2_a[1]), (coord2_b[0], coord2_b[1]), (255,255,255), thickness=6)
 
-		return slope_L, slope_R
+		# updating ROI
+		x_van,y_van = FindVanishingPoint(slope_L, slope_R, y_int_L, y_int_R)
+
+		return slope_L, slope_R, y_van
 	except Exception:
 		traceback.print_exc()
 		print("ERROR: No lines found")
-		return 0, 0
+		return 0, 0, 175
 
 
 def ProcessImage(image, vertices):    # only look at region of interest
@@ -94,6 +88,14 @@ def ProcessImage(image, vertices):    # only look at region of interest
 	masked = cv2.bitwise_and(image_edges, mask)
 
 	return masked
+
+
+def FindVanishingPoint(slope1, slope2, y_int1, y_int2):
+	x = (y_int2 - y_int1)/(slope1 - slope2)
+	y = slope1*x + y_int1
+
+	return x,y
+
 
 
 def PopulateLineLists(coords, lineAggregator_L, lineAggregator_R):
@@ -211,7 +213,7 @@ def LineFilter(lineAggregator, lineCounts):  # remove unwanted lines
 	coord_a = [int(x_bottom), int(height)]
 	coord_b = [int(x_top), 0]
 
-	return coord_a, coord_b, slope_avg
+	return coord_a, coord_b, slope_avg, y_int_avg
 
 
 ############## Driving functions  ####################
@@ -253,7 +255,8 @@ def main():
 	last_time = time.time()
 
 	WIDTH,HEIGHT = pag.size()
-	vertices = np.array([[0,800], [0,400], [WIDTH/8, 175], [3*WIDTH/8,175], [WIDTH/2,400], [WIDTH/2,800],  [9*WIDTH/32, 325], [7*WIDTH/32, 325]], np.int32)
+	y_van = 175
+	#vertices = np.array([[0,800], [0,350], [WIDTH/8, y_van], [3*WIDTH/8,y_van], [WIDTH/2,350], [WIDTH/2,800],  [9*WIDTH/32, 350], [7*WIDTH/32, 350]], np.int32)
 
 	for i in range(0,3):
 		print("On the count of 3: {}" .format(i))
@@ -262,6 +265,7 @@ def main():
 	while(True):
 			# 800x600 windowed mode
 			# bbox(x, y, width, height)
+			vertices = np.array([[0, 800], [0, 350], [WIDTH / 8, y_van], [3 * WIDTH / 8, y_van], [WIDTH / 2, 350], [WIDTH / 2, 800], [9 * WIDTH / 32, 350], [7 * WIDTH / 32, 350]], np.int32)
 
 			if police_dash == True:
 				image = np.array(ImageGrab.grab(bbox=(0, HEIGHT/4, WIDTH/2, 800)))  # grabbing screen into a numpy array  // for police dash
@@ -274,7 +278,7 @@ def main():
 			processed_image = ProcessImage(image, [vertices])
 			#cv2.imshow("Image", np.hstack([image,image2]))
 
-			slope_L, slope_R = Lines(processed_image)
+			slope_L, slope_R, y_van = Lines(processed_image)
 
 			if drive:
 				release_all_controls()

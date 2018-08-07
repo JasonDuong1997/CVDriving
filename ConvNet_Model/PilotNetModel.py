5768927bdfe2729f9d084dbe01bb3378f58ad9d1
JasonDuong1997 <jasonduong1997@gmail.com>
2018-08-06 18:14:13 -0800

    -added dropout and batch normalization layers

--- ConvNet_Model/PilotNetModel.py
+++ ConvNet_Model/PilotNetModel.py
@@ -1,10 +1,7 @@
 import tensorflow as tf
 import tflearn
 
-# Process
-"""
-input -> (convolution -> max pooling) ->  fully connected layer -> output layer
-"""
+is_training = True
 
 def conv2d(x, weight, bias, strides=1):
 	conv = tf.nn.conv2d(x, weight, strides=[1, strides,strides, 1], padding="SAME")
@@ -23,6 +20,7 @@ def dropout(x, drop_rate=0.5):
 def PilotNet_Model(x, WIDTH, HEIGHT, n_outputs, pool_s=2):
 	W_fc_input = 8*10
 	print(W_fc_input*64)
+
 	# DEFINING WEIGHTS
 	# Conv (conv): [filter_width, filter_height, channels, # of filters]
 	# FC (fc)    : [size of downsampled image * size of layer input, # of neurons in layer]
@@ -53,28 +51,33 @@ def PilotNet_Model(x, WIDTH, HEIGHT, n_outputs, pool_s=2):
 	# DEFINING PilotNet ARCHITECTURE
 	# Input Image(width = 80, height = 62, YUV) ->
 	# Normalization ->
-	# Convolution(5x5) -> Relu ->
-	# Convolution(5x5) -> Relu ->
-	# Convolution(5x5) -> Relu ->
+	# Convolution(5x5) -> Relu -> Normalization -> Relu ->
+	# Convolution(5x5) -> Relu -> Normalization -> Relu ->
+	# Convolution(5x5) -> Relu -> Normalization -> Relu ->
 	# Convolution(3x3) -> Relu ->
 	# Convolution(3x3) -> Relu ->
-	# Fully Connected Layer(1164) ->
-	# Fully Connected Layer(100) ->
-	# Fully Connected Layer(50) ->
-	# Output -> Steering Commands
+	# Fully Connected Layer(1164) -> Relu -> Dropout
+	# Fully Connected Layer(100) -> Relu -> Dropout
+	# Fully Connected Layer(50) -> Relu ->
+	# Output -> SoftMax	-> Steering Commands
 	x = tf.reshape(x, shape=[-1, HEIGHT, WIDTH, 3])
 	print("Input Size: {}" .format(x.get_shape()))
 
-	conv1 = conv2d(x, W_conv1, B_conv1, strides=2)
+	normalized = relu(tf.layers.batch_normalization(x, training=is_training))
+
+	conv1 = conv2d(normalized, W_conv1, B_conv1, strides=2)
 	conv1 = relu(conv1)
+	conv1 = relu(tf.layers.batch_normalization(conv1, training=is_training))
 	print("Conv1 Size: {}" .format(conv1.get_shape()))
 
 	conv2 = conv2d(conv1, W_conv2, B_conv2, strides=2)
 	conv2 = relu(conv2)
+	conv2 = relu(tf.layers.batch_normalization(conv2, training=is_training))
 	print("Conv2 Size: {}" .format(conv2.get_shape()))
 
 	conv3 = conv2d(conv2, W_conv3, B_conv3, strides = 2)
 	conv3 = relu(conv3)
+	conv3 = relu(tf.layers.batch_normalization(conv3, training=is_training))
 	print("Conv3 Size: {}" .format(conv3.get_shape()))
 
 	conv4 = conv2d(conv3, W_conv4, B_conv4, strides = 1)
@@ -88,12 +91,15 @@ def PilotNet_Model(x, WIDTH, HEIGHT, n_outputs, pool_s=2):
 	# flatten to 1 dimension for fully connected layers
 	flat_img = tf.reshape(conv5, shape=[-1, W_fc1.get_shape().as_list()[0]])
 	print("Reshape Size: {}" .format(flat_img.get_shape()))
+
 	fc1 = relu(tf.matmul(flat_img, W_fc1) + B_fc1)
-	fc1 = dropout(fc1, 0.4)
-	print("FC1 Size: {}" .format(fc1.get_shape()))
+	fc1 = dropout(fc1, 0.5)
+
 	fc2 = relu(tf.matmul(fc1, W_fc2) + B_fc2)
-	fc2 = dropout(fc2, 0.4)
+	fc2 = dropout(fc2, 0.5)
+
 	fc3 = relu(tf.matmul(fc2, W_fc3) + B_fc3)
+	fc3 = dropout(fc3, 0.2)
 
 	output = tf.matmul(fc3, W_out) + B_out
 
--- ConvNet_Model/TrainModel.py
+++ ConvNet_Model/TrainModel.py
@@ -8,10 +8,10 @@ data_version = "yuv"
 training_data = np.load("training_data_yuv_balanced.npy")
 testing_data = np.load("test_data_yuv.npy")
 
-learning_rate = 5e-4
+learning_rate = 1e-3
 test_size = int(len(training_data)*0.12)
 batch_size = 128  	# number of images per cycle (in the power of 2 because # of physical processors is similar)
-n_epochs = 180	 	# number of epochs
+n_epochs = 100	 	# number of epochs
 n_outputs = 3	  	# number of outputs
 pool_s = 2			# maxpool stride
 
@@ -33,8 +33,13 @@ def ConvNN_Train(x):
 	else:
 		prediction = ConvNN_Model(x, WIDTH, HEIGHT, n_outputs, pool_s)
 
+	# OPERATIONS
 	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction, labels=y))
-	optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+	# optimizer with normalization
+	update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+	with tf.control_dependencies(update_ops):
+		optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 	correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
 	accuracy = tf.reduce_mean(tf.cast(correct, "float"))
@@ -59,6 +64,7 @@ def ConvNN_Train(x):
 	print("train/test X: {}, {}" .format(len(train_x), len(test_x)))
 	print("train/test Y: {}, {}" .format(len(train_y), len(test_y)))
 
+	# dynamic allocation of GPU memory
 	config = tf.ConfigProto()
 	config.gpu_options.allow_growth = True
 
@@ -72,13 +78,7 @@ def ConvNN_Train(x):
 			running_acc = 0
 			for batch in range(int(len(train_x)/batch_size)):
 				batch_x = train_x[batch*batch_size:min((batch+1)*batch_size, len(train_x)-1)]
-
 				batch_y = train_y[batch*batch_size:min((batch+1)*batch_size, len(train_y)-1)]
-
-				"""
-				batch = sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
-				loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_x, y: batch_y})
-				"""
 
 				opt, loss = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
 				running_acc += sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
@@ -87,8 +87,9 @@ def ConvNN_Train(x):
 
 			epoch_acc = sess.run(accuracy, feed_dict={x: test_x, y: test_y})
 			print("Epoch {}/{}." .format(epoch+1, n_epochs))
-			print("Epoch Loss: {}, Epoch Accuracy: {}" .format(epoch_loss/test_size, epoch_acc))
+			print("Epoch Loss: {}, Epoch Accuracy: {}" .format(epoch_loss, epoch_acc))
 			print("Running Accuracy: {}" .format(running_acc/int(len(train_x)/batch_size)))
+
 		print("\nTraining Done!")
 		print("Final Accuracy: {}" .format(accuracy.eval({x: test_x, y: test_y})))
 
